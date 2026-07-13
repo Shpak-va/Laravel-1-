80180a6a8c71fa164e7f4ccef9787edb47e722e6
Shpak-va <victorshpak7@gmail.com>
2026-07-13 21:08:51 +0300

    Add files via upload

---
+++ database/seeders/FlightSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class FlightSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
