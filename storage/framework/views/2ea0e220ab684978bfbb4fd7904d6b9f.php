fb39dbbde855fbc3f421578924ad860a212862fa
Shpak-va <victorshpak7@gmail.com>
2026-07-13 21:17:55 +0300

    Add files via upload

---
+++ storage/framework/views/2ea0e220ab684978bfbb4fd7904d6b9f.php
@@ -0,0 +1,4 @@
+# Inertia + Svelte
+
+- IMPORTANT: Activate ___SINGLE_BACKTICK___inertia-svelte-development___SINGLE_BACKTICK___ when working with Inertia Svelte client-side patterns.
+<?php /**PATH /home/redos/example-app/storage/framework/views/962ce3bd13c09469d7f7c107b1c9e87b.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/962ce3bd13c09469d7f7c107b1c9e87b.blade.php
@@ -0,0 +1,3 @@
+# Inertia + Svelte
+
+- IMPORTANT: Activate ___SINGLE_BACKTICK___inertia-svelte-development___SINGLE_BACKTICK___ when working with Inertia Svelte client-side patterns.
